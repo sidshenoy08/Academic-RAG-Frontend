86f9c5c2456b4b1efa1a369cd6edd4ba0465d673
Siddharth Shenoy <sidshenoy2000@gmail.com>
2026-07-10 14:05:19 -0400

    Add d3.js charts for analytics to dashboard page

---
+++ app/dashboard/YearStackedBarplot.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useMemo, useRef } from "react";
+import * as d3 from "d3";
+
+const MARGIN = { top: 30, right: 30, bottom: 50, left: 50 };
+
+type Group = {
+    year: string;
+} & { [key: string]: number };
+
+type StackedBarplotProps = {
+    width: number;
+    height: number;
+    data: Group[];
+};
+
+const StackedBarplot = ({
+    width,
+    height,
+    data
+}: StackedBarplotProps) => {
+    // bounds = area inside the graph axis = calculated by substracting the margins
+
+    const axesRef = useRef(null);
+    const boundsWidth = width - MARGIN.right - MARGIN.left;
+    const boundsHeight = height - MARGIN.top - MARGIN.bottom;
+
+    const allGroups = data.map((d) => String(d.year));
+    const allSubgroups = ["totalLikes", "totalDislikes"];
+
+    // Data Wrangling: stack the data
+    const stackSeries = d3.stack().keys(allSubgroups).order(d3.stackOrderNone);
+    //.offset(d3.stackOffsetNone);
+    const series = stackSeries(data);
+
+    // Y axis
+    const max = 20; // todo
+    const yScale = useMemo(() => {
+        return d3
+            .scaleLinear()
+            .domain([0, max || 0])
+            .range([boundsHeight, 0]);
+    }, [data, height]);
+
+    // X axis
+    const xScale = useMemo(() => {
+        return d3
+            .scaleBand<string>()
+            .domain(allGroups)
+            .range([0, boundsWidth])
+            .padding(0.05);
+    }, [data, width]);
+
+    // Color Scale
+    var colorScale = d3
+        .scaleOrdinal<string>()
+        .domain(allGroups)
+        .range(["#3f8f29", "#de1a24"]);
+
+    // Render the X and Y axis using d3.js, not react
+    useEffect(() => {
+        const svgElement = d3.select(axesRef.current);
+        svgElement.selectAll("*").remove();
+        const xAxisGenerator = d3.axisBottom(xScale);
+        svgElement
+            .append("g")
+            .attr("transform", "translate(0," + boundsHeight + ")")
+            .call(xAxisGenerator);
+
+        const yAxisGenerator = d3.axisLeft(yScale);
+        svgElement.append("g").call(yAxisGenerator);
+    }, [xScale, yScale, boundsHeight]);
+
+    const rectangles = series.map((subgroup, i) => {
+        return (
+            <g key={i}>
+                {subgroup.map((group, j) => {
+                    return (
+                        <rect
+                            key={j}
+                            x={xScale(group.data.year)}
+                            y={yScale(group[1])}
+                            height={yScale(group[0]) - yScale(group[1])}
+                            width={xScale.bandwidth()}
+                            fill={colorScale(subgroup.key)}
+                            opacity={0.9}
+                        ></rect>
+                    );
+                })}
+            </g>
+        );
+    });
+
+    return (
+        <div>
+            <svg width={width} height={height}>
+                <g
+                    width={boundsWidth}
+                    height={boundsHeight}
+                    transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+                >
+                    {rectangles}
+                </g>
+                <g
+                    width={boundsWidth}
+                    height={boundsHeight}
+                    ref={axesRef}
+                    transform={`translate(${[MARGIN.left, MARGIN.top].join(",")})`}
+                />
+            </svg>
+        </div>
+    );
+};
+
+export default StackedBarplot;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,17 +5,61 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
+
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, SyntheticEvent } from 'react';
 
 import MonthBarplot from './MonthBarplot';
+import MonthStackedBarplot from './MonthStackedBarplot';
+import YearBarplot from './YearBarplot';
+import YearStackedBarplot from './YearStackedBarplot';
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 
 export default function Dashboard() {
     const [monthStats, setMonthStats] = useState([]);
     const [yearStats, setYearStats] = useState([]);
+    const [value, setValue] = useState(0);
 
     const router = useRouter();
+
+    const handleChange = (event: SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
 
     useEffect(() => {
         const getAnalytics = async () => {
@@ -64,22 +108,119 @@ export default function Dashboard() {
 
     return (
         <>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static" elevation={0} sx={{ bgcolor: 'transparent', color: 'black' }}>
-                    <Toolbar>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            QueryPi
-                        </Typography>
-                        <Button color="inherit" onClick={() => router.push("/home")}>
-                            Chats
-                        </Button>
-                        <Button color="inherit" onClick={logout}>
-                            Logout
-                        </Button>
-                    </Toolbar>
-                </AppBar>
-            </Box>
-            <MonthBarplot data={monthStats} width={400} height={400} />
+            {/* <Box sx={{ flexGrow: 1 }}> */}
+            <AppBar position="static" elevation={0} sx={{ bgcolor: 'transparent', color: 'black' }}>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        QueryPi
+                    </Typography>
+                    <Button color="inherit" onClick={() => router.push("/home")}>
+                        Chats
+                    </Button>
+                    <Button color="inherit" onClick={logout}>
+                        Logout
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            {/* </Box> */}
+            <Container maxWidth="xl" sx={{ py: 4 }}>
+                <Grid container spacing={3} sx={{ mt: 2, mb: 4 }}>
+                    <Grid size={{ xs: 12, md: 4 }}>
+                        <Paper elevation={3} sx={{ p: 2, minWidth: 150, textAlign: 'center' }}>
+                            <Typography variant="subtitle2" color="text.secondary">
+                                All-Time Questions Asked
+                            </Typography>
+                            <Typography variant="h3" color="primary" sx={{ fontWeight: 700 }}>
+                                {yearStats.reduce((accumulator, currentValue) => accumulator + currentValue.totalPrompts, 0).toLocaleString()}
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 4 }}>
+                        <Paper elevation={3} sx={{ p: 2, minWidth: 150, textAlign: 'center' }}>
+                            <Typography variant="subtitle2" color="text.secondary">
+                                Total Likes
+                            </Typography>
+                            <Typography variant="h3" color="primary" sx={{ fontWeight: 700 }}>
+                                {yearStats.reduce((accumulator, currentValue) => accumulator + currentValue.totalLikes, 0).toLocaleString()}
+                            </Typography>
+                        </Paper>
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 4 }}>
+                        <Paper elevation={3} sx={{ p: 2, minWidth: 150, textAlign: 'center' }}>
+                            <Typography variant="subtitle2" color="text.secondary">
+                                Total Dislikes
+                            </Typography>
+                            <Typography variant="h3" color="primary" sx={{ fontWeight: 700 }}>
+                                {yearStats.reduce((accumulator, currentValue) => accumulator + currentValue.totalDislikes, 0).toLocaleString()}
+                            </Typography>
+                        </Paper >
+                    </Grid>
+                </Grid >
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} centered textColor='primary' indicatorColor='primary'>
+                        <Tab label="Monthly Stats" {...a11yProps(0)} />
+                        <Tab label="Yearly Stats" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+                <CustomTabPanel value={value} index={0}>
+                    <Grid container spacing={3}>
+                        <Grid size={{ xs: 12, lg: 6 }}>
+                            <Paper sx={{ p: 2 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Total Questions Asked by Month
+                                </Typography>
+                                <MonthBarplot
+                                    data={monthStats}
+                                    width={550}
+                                    height={400}
+                                />
+                            </Paper>
+                        </Grid>
+
+                        <Grid size={{ xs: 12, lg: 6 }}>
+                            <Paper sx={{ p: 2 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Total Likes and Dislikes by Month
+                                </Typography>
+                                <MonthStackedBarplot
+                                    data={monthStats}
+                                    width={550}
+                                    height={400}
+                                />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </CustomTabPanel>
+
+                <CustomTabPanel value={value} index={1}>
+                    <Grid container spacing={3}>
+                        <Grid size={{ xs: 12, lg: 6 }}>
+                            <Paper sx={{ p: 2 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Total Questions Asked by Year
+                                </Typography>
+                                <YearBarplot
+                                    data={yearStats}
+                                    width={550}
+                                    height={400}
+                                />
+                            </Paper>
+                        </Grid>
+                        <Grid size={{ xs: 12, lg: 6 }}>
+                            <Paper sx={{ p: 2 }}>
+                                <Typography variant="h6" gutterBottom>
+                                    Total Likes and Dislikes by Year
+                                </Typography>
+                                <YearStackedBarplot
+                                    data={yearStats}
+                                    width={550}
+                                    height={400}
+                                />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </CustomTabPanel>
+            </Container>
         </>
     )
 }
